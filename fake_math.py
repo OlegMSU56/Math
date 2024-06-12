24087ef35e8a667e94aeb8a85e38d74cb11350b3
Oleg <sagdeev.ol@yandex.ru>
2024-06-12 18:42:48 +0300

    Update fake_math.py

--- fake_math.py
+++ fake_math.py
@@ -1,11 +1,6 @@
-def fake_divide(first, second):
+def divide(first, second):
     if second == 0:
         return 'Ошибка'
     if second !=0:
         div = first / second
         return div
-
-result1 = fake_divide(69, 3)
-result2 = fake_divide(3, 0)
-print(result1)
-print(result2)
